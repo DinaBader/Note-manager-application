f64c121c84afec1da04b4210987aba7b2160467e
Dina Bader <dina19bader@gmail.com>
2024-04-24 17:19:36 +0300

    Added delete confirmation

--- note-manager/pages/Index.js
+++ note-manager/pages/Index.js
@@ -1,25 +1,44 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { DeleteTodoAction } from '@/app/actions/TodoActions';
+import { ConfirmDialog } from 'primereact/confirmdialog';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
 
 function Index() {
     const router = useRouter();
     const Todo = useSelector((state) => state.Todo);
     const { todos } = Todo;
-    const dispatch =useDispatch();
+    const dispatch = useDispatch();
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+    const [todoToDelete, setTodoToDelete] = useState(null);
 
     const handleClick = () => {
         router.push('/add');
     };
 
-    const view=(id)=>{
+    const view = (id) => {
         router.push(`/view/${id}`);
     }
 
-    const deleteTodo=(id)=>{
-        dispatch(DeleteTodoAction(id));
+    const confirmDelete = (id) => {
+        setTodoToDelete(id);
+        setShowDeleteConfirmation(true);
+    }
+
+    const handleDelete = () => {
+        if (todoToDelete) {
+            dispatch(DeleteTodoAction(todoToDelete));
+            setTodoToDelete(null);
+            setShowDeleteConfirmation(false);
+        }
+    }
+
+    const hideDeleteConfirmation = () => {
+        setTodoToDelete(null);
+        setShowDeleteConfirmation(false);
     }
 
     return (
@@ -28,12 +47,22 @@ function Index() {
             <ul>
                 {todos && todos.map((todo) => (
                     <li key={todo.id}>
-                        <span onClick={()=>view(todo.id)}>{todo.todo}</span>
+                        <span onClick={() => view(todo.id)}>{todo.todo}</span>
                         <button>Edit</button>
-                        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+                        <button onClick={() => confirmDelete(todo.id)}>Delete</button>
                     </li>
                 ))}
             </ul>
+            
+            <ConfirmDialog 
+                visible={showDeleteConfirmation} 
+                onHide={hideDeleteConfirmation} 
+                message="Are you sure you want to delete this todo?" 
+                header="Confirmation"
+                icon="pi pi-exclamation-triangle"
+                accept={handleDelete}
+                reject={hideDeleteConfirmation}
+            />
         </div>
     );
 }
